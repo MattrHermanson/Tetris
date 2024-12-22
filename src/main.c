12824b513b327db535e657e24ad118d87e0552dd
MattrHermanson <matth2004@outlook.com>
2024-12-22 14:33:16 -0800

    now there is a square that moves left and right

--- src/main.c
+++ src/main.c
@@ -1,26 +1,37 @@
 #include "raylib.h"
 
-int main(void) {
+int main (void) {
 
   const int screenWidth = 800;
   const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+  InitWindow(screenWidth, screenHeight, "Drawing a Square");
 
   SetTargetFPS(60);
 
-  while(!WindowShouldClose()) {
+  int rectX = 400;
+  int rectY = 225;
 
+  while (!WindowShouldClose()) {
+    
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
+    DrawText("Here is a square", 190, 200, 20, BLACK);
 
-    DrawText("Hello World", 190, 200, 20, LIGHTGRAY);
+    if (IsKeyDown(KEY_A)) {
+      rectX -= 10;
+    }
+
+    if (IsKeyDown(KEY_D)) {
+      rectX += 10;
+    }
+
+    DrawRectangle(rectX, rectY, 40, 40, BLACK);
 
     EndDrawing();
   }
 
   CloseWindow();
-
   return 0;
 }
