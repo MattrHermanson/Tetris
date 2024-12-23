7401bb3b1259882077d6ed76747e549ec05ba131
MattrHermanson <matth2004@outlook.com>
2024-12-22 21:06:26 -0800

    added createGameGrid and drawGameGrid, not working right now

--- src/main.c
+++ src/main.c
@@ -1,47 +1,72 @@
 #include "raylib.h"
 
+#define gridWidth 5
+#define gridColor GRAY
+
+typedef struct GridLine {
+  Vector2 pos;
+  int width;
+  Color color;
+} GridLine;
+
+void createGameGrid(GridLine* grid, int rows, int cols, int size);
+void drawGameGrid(GridLine* grid, int rows, int cols);
+
 int main (void) {
 
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+  const int screenWidth = 400;
+  const int screenHeight = 600;
+
+  GridLine gameGrid[23];
+  createGameGrid(gameGrid, 15, 10, 40);
+
+  //40x40 blocks
 
   InitWindow(screenWidth, screenHeight, "Drawing a Square");
 
   SetTargetFPS(60);
 
-  Rectangle floor = {200, 300, 300, 10};
-  Rectangle player = {400, 225, 40, 40};
-
 
   while (!WindowShouldClose()) {
     
-    BeginDrawing();
+    BeginDrawing(); 
 
     ClearBackground(RAYWHITE);
-    DrawText("Here is a square", 190, 200, 20, BLACK);
+    drawGameGrid(gameGrid, 15, 10);
 
-    if (IsKeyDown(KEY_A)) {
-      player.x -= 10;
-    }
-
-    if (IsKeyDown(KEY_D)) {
-      player.x += 10;
-    }
-
-    if (IsKeyPressed(KEY_SPACE)) {
-      player.y -= 50;
-    }
-
-    if (CheckCollisionRecs(player, floor) == false) {
-      player.y += 1;
-    }
-
-    DrawRectangle(floor.x, floor.y, floor.width, floor.height, GRAY);
-    DrawRectangle(player.x, player.y, 40, 40, BLACK);
 
     EndDrawing();
   }
 
   CloseWindow();
   return 0;
+}
+
+void createGameGrid (GridLine* grid, int rows, int cols, int size) {
+
+  //create column grid lines
+  for (int i = 0; i < (rows-1); i++) {
+    int xOffset = i * size;
+    GridLine newLine = {{xOffset, 0}, gridWidth, gridColor};
+    grid[i] = newLine;
+  }
+
+  //create row grid lines
+  for (int i = 0; i < (cols-1); i++) {
+    int yOffset = i * size;
+    GridLine newLine = {{0, yOffset}, gridWidth, gridColor};
+    grid[i] = newLine;
+  }
+}
+
+//TODO not drawing grid the right way
+void drawGameGrid (GridLine* grid, int rows, int cols) {
+
+  for (int i = 0; i < (rows-1); i++) {
+    DrawRectangle(grid[i].pos.x, grid[i].pos.y, 400, grid[i].width, grid[i].color);
+  }
+
+  for (int i = 0; i < (cols-1); i++) {
+    DrawRectangle(grid[i].pos.x, grid[i].pos.y, grid[i].width, 600, grid[i].color);
+  }
 }
