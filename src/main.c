90d6c800cb1a4d9f3dae49278e2986256ba28969
MattrHermanson <matth2004@outlook.com>
2024-12-24 10:33:39 -0800

    grid bug now fixed and drawing correctly

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
+#include "stdio.h"
 
-#define gridWidth 5
+#define gridWidth 2
 #define gridColor GRAY
 
 typedef struct GridLine {
@@ -45,28 +46,29 @@ int main (void) {
 void createGameGrid (GridLine* grid, int rows, int cols, int size) {
 
   //create column grid lines
-  for (int i = 0; i < (rows-1); i++) {
-    int xOffset = i * size;
-    GridLine newLine = {{xOffset, 0}, gridWidth, gridColor};
-    grid[i] = newLine;
-  }
-
-  //create row grid lines
-  for (int i = 0; i < (cols-1); i++) {
+  for (int i = 0; i < rows; i++) {
     int yOffset = i * size;
     GridLine newLine = {{0, yOffset}, gridWidth, gridColor};
     grid[i] = newLine;
   }
+
+  for (int j = 0; j < cols; j++) {
+    int xOffset = j * size;
+    GridLine newLine = {{xOffset, 0}, gridWidth, gridColor};
+    grid[j + rows] = newLine;
+  }
+
 }
 
-//TODO not drawing grid the right way
 void drawGameGrid (GridLine* grid, int rows, int cols) {
 
-  for (int i = 0; i < (rows-1); i++) {
+  for (int i = 0; i < (rows); i++) {
     DrawRectangle(grid[i].pos.x, grid[i].pos.y, 400, grid[i].width, grid[i].color);
   }
-
-  for (int i = 0; i < (cols-1); i++) {
+ 
+  int endBound = rows + cols;
+  for (int i = rows; i <  endBound; i++) {
     DrawRectangle(grid[i].pos.x, grid[i].pos.y, grid[i].width, 600, grid[i].color);
   }
+
 }
